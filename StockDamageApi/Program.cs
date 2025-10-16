aaefbf59ba9e0f5314ccbc0ffb4c35a02dd68968
2eg4rizve <2eg4rizve@gmail.com>
2025-10-16 21:28:24 +0600

    program.cs update

--- StockDamageApi/Program.cs
+++ StockDamageApi/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using StockDamageApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +12,17 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// ✅ Enable CORS for Blazor frontend
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Swagger in development
@@ -22,6 +33,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// ✅ Enable CORS middleware
+app.UseCors();
+
 app.UseAuthorization();
 app.MapControllers();
+
 app.Run();
